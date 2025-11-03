40701fc1750a20f174882e584c2371bcf79768fb
alexkallai <scythe1092@gmail.com>
2025-11-03 20:44:28 +0100

    Added basic layout

---
+++ Data Analyzer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Data_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

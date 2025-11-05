7aaa76f7db3945bb9f17000f5fd77352d16754d1
Alex Kallai <alex.kallai@dmgmori-heitec.hu>
2025-11-05 09:54:08 +0100

    Added Training sample program

---
+++ Basic test repo/Program.cs
@@ -0,0 +1,46 @@
+﻿int number1;
+int number2;
+
+
+while (true)
+{
+    Console.WriteLine("Please type the first number!");
+    number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Please type the second number!");
+    number2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Please select the operation:");
+    Console.WriteLine("[A]dd numbers");
+    Console.WriteLine("[S]ubtract numbers");
+    Console.WriteLine("[M]multiply numbers");
+
+    while (true)
+    {
+        char selection = Convert.ToChar(Console.ReadLine()[0]);
+        bool finished = false;
+        switch (selection)
+        {
+            case 'A':
+                Console.WriteLine(number1 + number2);
+                finished = true;
+                break;
+            case 'S':
+                Console.WriteLine(number1 - number2);
+                finished = true;
+                break;
+            case 'M':
+                Console.WriteLine(number1 * number2);
+                finished = true;
+                break;
+            default:
+                Console.WriteLine("Incorrect input, please retry!");
+                break;
+        }
+        if (finished)
+        {
+            Console.WriteLine("Restarting! \n");
+            break;
+        }
+    }
+
+}

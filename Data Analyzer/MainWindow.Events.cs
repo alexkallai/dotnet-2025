15b265aeb1bc437c3aa42601d38b2fa926b7fa65
Alex Kallai <alex.kallai@dmgmori-heitec.hu>
2025-11-06 15:21:35 +0100

    Added partial class extension

---
+++ Data Analyzer/MainWindow.Events.cs
@@ -0,0 +1,21 @@
+﻿using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using ScottPlot.Panels;
+using ScottPlot.WPF;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+
+namespace Data_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+    }
+}
+  

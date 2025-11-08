300d5c6b1abb2743307458523a40bbfd66edb58d
Alex Kallai <alex.kallai@dmgmori-heitec.hu>
2025-11-08 09:17:51 +0100

    Added new tool skeleton

---
+++ Network tools/NetworkTools.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_tools
+{
+    public class NetworkTools
+    {
+        public static List<InterFace> interfaces = new List<InterFace>();
+        public struct InterFace
+        {
+            public string Name;
+            public string IP;
+            public string subnetMask;
+            public string[] defaultGateway;
+        }
+        public NetworkTools() {
+            EnumerateInterfaces();
+        }
+
+        public void EnumerateInterfaces()
+        {
+
+            foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                InterFace interFace = new InterFace();
+                // Filter for operational network interfaces
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    Console.WriteLine($"Interface: {networkInterface.Name}");
+                    IPInterfaceProperties properties = networkInterface.GetIPProperties();
+
+                    // Get IP addresses
+                    foreach (UnicastIPAddressInformation ip in properties.UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine($"  IP Address: {ip.Address}");
+                            Console.WriteLine($"  Subnet Mask: {ip.IPv4Mask}");
+                            interFace.Name = networkInterface.Name;
+                            interFace.IP = ip.Address.ToString();
+                            interFace.subnetMask = ip.IPv4Mask.ToString();
+                        }
+                    }
+
+                    // Get default gateway
+                    foreach (GatewayIPAddressInformation gateway in properties.GatewayAddresses)
+                    {
+                        Console.WriteLine($"  Default Gateway: {gateway.Address}");
+                        
+                    }
+
+                    Console.WriteLine();
+                }
+                interfaces.Add(interFace);
+            }
+            }
+        }
+
+}
---
+++ Network tools/Program.cs
@@ -0,0 +1,97 @@
+﻿using Network_tools;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using static Network_tools.NetworkTools;
+
+class Program
+{
+    static Stack<Action> menuStack = new Stack<Action>();
+    static NetworkTools networkTools = new NetworkTools();
+
+    static void Main(string[] args)
+    {
+        // Start with the main menu
+        menuStack.Push(MainMenu);
+        menuStack.Peek().Invoke(); // Display the top menu
+    }
+
+    static void MainMenu()
+    {
+        Console.Clear();
+        var feature = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("[bold yellow]Select a feature:[/]")
+                .AddChoices(new[] { "Network Scan", "Coming up", "Exit" })
+        );
+
+        switch (feature)
+        {
+            case "Network Scan":
+                menuStack.Push(NetworkScanMenu);
+                menuStack.Peek().Invoke(); // Navigate to Network Scan menu
+                break;
+            case "Coming up...":
+                HandleComingUp();
+                menuStack.Peek().Invoke(); // Stay in the main menu
+                break;
+            case "Exit":
+                Environment.Exit(0); // Exit the application
+                break;
+        }
+    }
+
+    static void NetworkScanMenu()
+    {
+        Console.Clear();
+        var option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("[bold green]Select a network scan option:[/]")
+                .AddChoices(new[] { "List Network Interfaces", "Scan Active Interfaces", "Back" })
+        );
+
+        switch (option)
+        {
+            case "List Network Interfaces":
+                ListNetworkInterfaces();
+                menuStack.Peek().Invoke(); // Stay in the current menu
+                break;
+            case "Scan Active Interfaces":
+                ScanActiveInterfaces();
+                menuStack.Peek().Invoke(); // Stay in the current menu
+                break;
+            case "Back":
+                menuStack.Pop(); // Remove current menu from stack
+                menuStack.Peek().Invoke(); // Go back to the previous menu
+                break;
+        }
+    }
+
+    static void HandleComingUp()
+    {
+        Console.Clear();
+        AnsiConsole.MarkupLine("[bold red]Feature coming soon![/]");
+        Console.ReadKey();
+    }
+
+    static void ListNetworkInterfaces()
+    {
+        var names = interfaces.Select(iface => $"{iface.IP}, \t\t {iface.Name}").ToList();
+        Console.Clear();
+        AnsiConsole.MarkupLine("[bold yellow]Listing all network interfaces...[/]");
+        var selection = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("Found interfaces:")
+            .AddChoices(names));
+        // Add logic to list network interfaces
+        Console.ReadKey();
+    }
+
+    static void ScanActiveInterfaces()
+    {
+        Console.Clear();
+        AnsiConsole.MarkupLine("[bold green]Scanning active network interfaces...[/]");
+        // Add logic to scan active interfaces
+        Console.ReadKey();
+    }
+}
